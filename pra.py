2c876e896ddfe71a3bc8531080bc11a7de9428ca
Студент НИУ ВШЭ <student@basm-402-std23.staff.corp.local>
2017-09-19 11:56:15 +0300

    19.09 first lesson

---
+++ pra.py
@@ -0,0 +1,21 @@
+print ("Hello!")
+'''
+x=10
+y=x*100
+'''
+#print("x =",x)
+#print("y =", y)
+
+#human_name = input("Как вас зовут?")
+#print("Привет", human_name)
+#print(type(human_name))
+number = int(input('number, please:'))
+#number2= int(input('second one:'))
+#num1=int(number)
+#num2=int(number2)
+#print(number*number2)
+
+
+if number > 5:
+    print('Ваше число больше пяти')
+    print('!!!')
