583e2edee709235d51ec4505307d85860f3a2f83
Студент НИУ ВШЭ <student@basm-402-std25.staff.corp.local>
2017-09-26 11:48:55 +0300

    26/09 class

---
+++ class2.py
@@ -0,0 +1,73 @@
+"""
+input_num = input("Введите число:")
+num=int (input_num)
+#word_length = len(word)
+#print (word_length)
+"""
+"""
+#if num > 0:
+    #print (num)
+    #print ("Больше нуля")
+#else:
+    #print("Не больше нуля")
+    #if num > -2:
+     #print ("Больше -2")
+    #else:
+        #print ("Не больше -2")
+   # num = num + 100
+   #num += 100
+#print(num)
+
+
+#if num > 0 and num <= 5:
+    #print(num, "Строго больше нуля ИЛИ меньше или равно 5")
+#print("End.")
+
+#if not 2<3:
+   # print("hello")
+#else:
+   # print("Noooooo")
+if num % 2 == 0:
+    print (num, "четное")
+input_num2 = input("Введите число2:")
+num2=int (input_num2)
+if num2 % 2 == 0:
+    print (num2, "четное")
+"""
+"""
+x = input ("Введите число:")
+x = int(x)
+y = input("Введите число:")
+y = int(y)
+print (x,y)
+
+if x % 2 == 0 and y % 2 == 0:
+    print("оба четные")
+elif x % 2 == 0 and y % 2 != 0:
+    print ("х , но ну у")
+elif x % 2 != 0 and y % 2 == 0:
+    print ("у, но не х")
+    """
+"""
+x = input ("Введите число:")
+x = int(x)
+print(x)
+if x > 0:
+    print(1)
+elif x == 0:
+    print(0)
+else:
+    print (-1)
+    """
+year = input("Bведите год:")
+year = int(year)
+print(year)
+if len(year)
+
+if (year % 4 ==0 and year % 100 !=0) or (year % 400 == 0):
+    print ("Високосный")
+else:
+    print ("Невисокосный")
+   
+print("End.")
+        
