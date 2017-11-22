47411c37204461673cb7e945da8c6ab26a0c3553
arinavetoshk <31654751+arinavetoshk@users.noreply.github.com>
2017-11-22 21:44:57 +0300

    Add files via upload

--- hw4/hw4.py
+++ hw4/hw4.py
@@ -1,11 +1,9 @@
-#Арина Ветошкина
-#Вариант 2
-fname = "text.txt"
+fname = "C:\\Users\\Арина\\Desktop\\text.txt"
 with open(fname, encoding="utf-8") as f:
     alist = [line.rstrip() for line in f]
-len_str1 =  len(max(alist,key = len).split())
-len_str2 =  len(min(alist,key = len).split())
-print("Самая  длинная строка: ", len_str1)
-print ("Самая короткая строка: ", len_str2)
-m = len_str1/len_str2
+len_max =  len(max(alist,key = len).split())
+len_min =  len(min(alist,key = len).split())
+print("Самая  длинная строка: ", len_max)
+print ("Самая короткая строка: ", len_min)
+m = len_max/len_min
 print("Самая короткая строка меньше самой длинной в", ("%.2f" % m), "раз(-а).")
