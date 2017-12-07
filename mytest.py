b8e935d4ebb690d2ae15393a45f7d5fe33a52e3c
arinavetoshk <31654751+arinavetoshk@users.noreply.github.com>
2017-12-07 12:13:03 +0300

    Update mytest.py

--- mytest.py
+++ mytest.py
@@ -1,14 +1,22 @@
 #Вариант1
+'''
 #1
 fname = "C:\\Users\\Арина\\Desktop\\quotes.txt"
-ql = []
 with open(fname, encoding="utf-8") as f:
     quote = f.readlines()
-    #ql.append(quote)
 for line in quote:
     word = line.split()
 for q in quote:
     if len(quote) > 10 and len(q.split(' ', 1)[0]) < 5:
         print(q)
-
-    
+'''
+#2
+fname = "C:\\Users\\Арина\\Desktop\\quotes.txt"
+wl = []
+with open(fname, encoding="utf-8") as f:
+    for line in f:
+        for part in line.split():
+            if "разум" in part:
+                wl.append(line)
+print (len(wl))
+  
